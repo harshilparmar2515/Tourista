6d24370d034caeac905725fd6f96effb9cab8a31
harshilparmar2515 <harshilparmar2515@gmail.com>
2026-03-21 20:08:06 +0530

    Update Firebase.js

--- Vite Project/src/Firebase/Firebase.js
+++ Vite Project/src/Firebase/Firebase.js
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_,
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.projectId,
   storageBucket: import.meta.env.storageBucket,
